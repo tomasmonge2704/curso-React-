7466287c0e5ad1c732b5db01e8785bd3739a6ec8
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2021-12-17 21:51:36 -0300

    fix ItemCount

---
+++ src/components/ItemCount.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ExposurePlus1Icon from '@material-ui/icons/ExposurePlus1';
+import ExposureNeg1Icon from '@material-ui/icons/ExposureNeg1';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+
+
+export default function ItemCount({stock}){
+    
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          maxWidth: 345,
+        },
+        
+      }));
+      const classes = useStyles();
+    const [count, onAdd] = useState(0);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
+      const [anchorEl, setAnchorEl] = React.useState(null);
+     
+    
+      const open = Boolean(anchorEl);
+      const id = open ? 'simple-popover' : undefined;
+    return (
+        <div>
+        <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group" >
+            <Button className="botonMenos" onClick={() =>{if (count !== 0){onAdd(count - 1)}else{console.log("no podes tener negativo");}}}><ExposureNeg1Icon/></Button>
+            <Button>{count}</Button>
+            <Button className="botonMas" onClick={() => {if (stock == count){console.log("no stock");} else{onAdd(count + 1)}}}><ExposurePlus1Icon/></Button>
+            <Button variant="contained" color="secondary" onClick={handleClick}>
+            Ver mas
+          </Button>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <Typography className={classes.typography}>lorem sadasdasdasdasfsd asfaasda dsa dasd asdas da</Typography>
+          </Popover>
+          </ButtonGroup>
+          
+          <Button variant="outlined" color="secondary" className='BotonCarrito' style={{ width:"100%" }}>
+            Agregar al carrito 
+          </Button>
+          </div>
+    )
+    
+}
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,6 @@
 
 import arr from "./Item";
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -9,12 +9,8 @@ import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import { red } from '@material-ui/core/colors';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import ExposurePlus1Icon from '@material-ui/icons/ExposurePlus1';
-import ExposureNeg1Icon from '@material-ui/icons/ExposureNeg1';
 import Typography from '@material-ui/core/Typography';
-import Popover from '@material-ui/core/Popover';
+import ItemCount from "./ItemCount";
 
 
 export default function ItemList({stock}) {
@@ -44,18 +40,7 @@ export default function ItemList({stock}) {
         },
       }));
       const classes = useStyles();
-      const [count, onAdd] = useState(0);
-      const [anchorEl, setAnchorEl] = React.useState(null);
-      const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
     
-      const handleClose = () => {
-        setAnchorEl(null);
-      };
-    
-      const open = Boolean(anchorEl);
-      const id = open ? 'simple-popover' : undefined;
       return (
         <div style={{ width:"100%", justifyContent:'center', display:'flex',}}>
         {arr.map(prod => <Card className={classes.root} style={{ marginRight:"3%"}} >
@@ -78,34 +63,8 @@ export default function ItemList({stock}) {
           <CardContent>
           <Typography variant="h6">Stock disponible: {stock}</Typography>
           <Typography variant="h6">precio: ${prod.price}</Typography>
-          <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group" >
-            <Button className="botonMenos" onClick={() =>{if (count !== 0){onAdd(count - 1)}else{console.log("no podes tener negativo");}}}><ExposureNeg1Icon/></Button>
-            <Button>{count}</Button>
-            <Button className="botonMas" onClick={() => {if (stock == count){console.log("no stock");} else{onAdd(count + 1)}}}><ExposurePlus1Icon/></Button>
-            <Button variant="contained" color="secondary" onClick={handleClick}>
-            Ver mas
-          </Button>
-          <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'center',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-          >
-            <Typography className={classes.typography}>lorem sadasdasdasdasfsd asfaasda dsa dasd asdas da</Typography>
-          </Popover>
-          </ButtonGroup>
+          <ItemCount/>
           
-          <Button variant="outlined" color="secondary" className='BotonCarrito' style={{ width:"100%" }}>
-            Agregar al carrito 
-          </Button>
     
           </CardContent>
           
