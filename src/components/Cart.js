06c3dc57f60548938a3997f4224320df5d80980f
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2022-01-08 20:28:41 -0300

    update clear()

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,15 @@
-import {cartContext, clear} from "./CartContext"
-import React , {useContext} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Button from '@material-ui/core/Button';
+import { cartContext, clear, clearEspecifico } from "./CartContext";
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
 const useStyles = makeStyles({
   table: {
     minWidth: 700,
@@ -20,77 +20,80 @@ export default function Cart() {
   const classes = useStyles();
   const productosAgregados = useContext(cartContext);
 
-  function Subtotal (){
+  function Subtotal() {
     let subtotal = 0;
-    productosAgregados.map(prodData => (
-      subtotal = subtotal + (prodData.price * prodData.cantidad)
-    ))
-    return subtotal
+    productosAgregados.map(
+      (prodData) => (subtotal = subtotal + prodData.price * prodData.cantidad)
+    );
+    return subtotal;
   }
-  function borrarRow(title){
-   
-    console.log(title);
-    }
-  function borrarTodo(){
+  function borrarRow(title) {
+    document.getElementById(title).remove();
+    clearEspecifico(productosAgregados, title);
+  }
+  function borrarTodo() {
+    [...document.getElementsByClassName("productosRow")].map(n => n && n.remove());
     clear(productosAgregados);
-    console.log(productosAgregados);
   }
-  return(
-      
-   <div>
-      
-     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-          </TableRow>
-          <TableRow>
-            <TableCell>Desc</TableCell>
-            <TableCell align="right">Precio/u</TableCell>
-            <TableCell align="right">Unit</TableCell>
-            <TableCell align="right">Suma</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {productosAgregados.map(prodData => (
-            <TableRow key={prodData.title}>
-               
-              <TableCell><Button onClick={borrarRow.bind(this, prodData.title)}
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        startIcon={<DeleteIcon/>}
-      ></Button>  {prodData.title}</TableCell>
-              <TableCell align="right">${prodData.price}</TableCell>
-              <TableCell align="right">{prodData.cantidad}</TableCell>
-              <TableCell align="right">${prodData.price * prodData.cantidad}</TableCell>
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="spanning table">
+          <TableHead>
+            <TableRow></TableRow>
+            <TableRow>
+              <TableCell>Desc</TableCell>
+              <TableCell align="right">Precio/u</TableCell>
+              <TableCell align="right">Unit</TableCell>
+              <TableCell align="right">Suma</TableCell>
             </TableRow>
-          ))}
-          <Button onClick={borrarTodo}
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        startIcon={<DeleteIcon/>}
-      >Borrar todo</Button>
-          <TableRow>
-            <TableCell rowSpan={3} />
-            <TableCell colSpan={2}>Subtotal</TableCell>
-            <TableCell align="right">${Subtotal()}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Tax</TableCell>
-            <TableCell align="right">{`21 %`}</TableCell>
-            <TableCell align="right">${Subtotal() * 0.21}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell colSpan={2}>Total</TableCell>
-            <TableCell align="right">${Subtotal() * 1.21}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-  
-        
-   </div>
-  ) 
+          </TableHead>
+          <TableBody>
+            {productosAgregados.map((prodData) => (
+              <TableRow className={"productosRow"} id={prodData.title} key={prodData.title}>
+                <TableCell>
+                  <Button
+                    onClick={borrarRow.bind(this, prodData.title)}
+                    variant="contained"
+                    color="secondary"
+                    className={classes.button}
+                    startIcon={<DeleteIcon />}
+                  ></Button>{" "}
+                  {prodData.title}
+                </TableCell>
+                <TableCell align="right">${prodData.price}</TableCell>
+                <TableCell align="right">{prodData.cantidad}</TableCell>
+                <TableCell align="right">
+                  ${prodData.price * prodData.cantidad}
+                </TableCell>
+              </TableRow>
+            ))}
+            <Button
+              onClick={borrarTodo}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Borrar todo
+            </Button>
+            <TableRow>
+              <TableCell rowSpan={3} />
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell align="right">${Subtotal()}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Tax</TableCell>
+              <TableCell align="right">{`21 %`}</TableCell>
+              <TableCell align="right">${Subtotal() * 0.21}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell align="right">${Subtotal() * 1.21}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
+export const cartContext = React.createContext([]);
 
-export const cartContext = React.createContext([])
-
-export function addItem(){
-   
+export function addItem() {}
+export function clearEspecifico(productosAgregados, title) {
+  for (let i = 0; i < productosAgregados.length; i++) {
+    if (productosAgregados[i].title.trim() === title.trim()) {
+      productosAgregados.splice(i, 1);
+    }
+  }
 }
-
-export function clear(productosAgregados){
-   
-    productosAgregados.splice(0, productosAgregados.length);
+export function clear(productosAgregados) {
+  productosAgregados.splice(0, productosAgregados.length);
 }
-
