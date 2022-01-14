6344b78eaf8f5eee04de642dd5d9aaf9fe604494
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2022-01-14 05:04:12 -0300

    fixed category

--- src/components/ItemDetalContainer.js
+++ src/components/ItemDetalContainer.js
@@ -1,5 +1,4 @@
 import ItemDetail from "./ItemDetail";
-import { useState} from "react";
 import { useParams } from "react-router-dom";
 import { data } from "./datos";
 import '../App.css';
@@ -9,26 +8,14 @@ import '../App.css';
 
 export default function ItemDetailContainer() {
     
-    const [product, setProduc] = useState([])
+    
     const {Id} = useParams();
     const itemBuscado = data.find( item => item.id === Id);
     
-    const GetItem = () => {
-        
-        new Promise((resolve, reject)=> {
-            
-            setTimeout(()=>{
-               resolve(setProduc(itemBuscado))
-            }, 2000);
-           
-        });
-       return(product)
-        
-    }
     
     return (
         <div style={{ width:"100%", justifyContent:'center', display:'flex', marginTop:'5%'}}>
-        <ItemDetail item={GetItem()}/>
+        <ItemDetail item={itemBuscado}/>
         </div>
      )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,21 +1,31 @@
 import ItemList from "./ItemList";
 import { useState, useEffect } from "react";
+import {getDocs, getFirestore, collection} from "firebase/firestore"
 import { useParams } from "react-router-dom";
-import { data } from "./datos";
-import {getDocs, getFirestore, doc, collection} from "firebase/firestore"
 
 export default function ItemListContainer() {
-  const [stateData, setProduct] = useState([])
+  const [data, setData] = useState([])
+  const {categoryId} = useParams();
+ 
   useEffect(()=>{
     const db = getFirestore();
     const itemsCollection = collection(db, "items");
     getDocs(itemsCollection).then((snapshot)=>{ 
-        setProduct(snapshot.docs.map((doc)=>({id: doc.id,...doc.data()})));
+      setData(snapshot.docs.map((doc)=>({id: doc.id,...doc.data()})));
     });
   }, []);
     
+  useEffect(()=>{
+    return (categoryId)
+  },[categoryId])
+  
+  let categoryProducts = data.filter( category => category.categoria === categoryId)
+  if(categoryId === undefined) {
+    categoryProducts = data;
+  }
+     
 
 
 
-  return <ItemList Products={stateData} />;
+  return <ItemList Products={categoryProducts} />;
 }
